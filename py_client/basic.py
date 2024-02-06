c16d19be4960c08a3143ae948eb52c234add9e81
Robert Gleason <l2ob122@gmail.com>
2024-02-05 20:52:50 -0800

    Add request data logging and update API response***

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,6 +4,8 @@ import requests
 # endpoint = "http://httpbin.org/anything"
 endpoint = "http://localhost:8000/api"
 
-get_response = requests.get(endpoint)
-print(get_response.text)  # print raw response
-# print(get_response.json())  # print response as json
+get_response = requests.get(
+    endpoint, params={"abc": 123}, json={"query": "Hello, World!"}
+)
+# print(get_response.text)  # print raw response
+print(get_response.json())  # print response as json
