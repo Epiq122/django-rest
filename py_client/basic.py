1a13cbb09000ae78381c791b6a8ccb2a4b7242cb
Robert Gleason <l2ob122@gmail.com>
2024-02-05 19:41:24 -0800

    Update endpoint URL in basic.py

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,9 @@
 import requests
 
-endpoint = "http://httpbin.org/status/200"
-endpoint = "http://httpbin.org/anything"
+# endpoint = "http://httpbin.org/status/200"
+# endpoint = "http://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
 
 get_response = requests.get(endpoint)
 print(get_response.text)  # print raw response
-print(get_response.json())  # print response as json
+# print(get_response.json())  # print response as json
