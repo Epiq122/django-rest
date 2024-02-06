4257f20a4e18601e464a6bc6b9505d16611853d2
Robert Gleason <l2ob122@gmail.com>
2024-02-05 19:49:14 -0800

    Add API files and endpoints

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 # endpoint = "http://httpbin.org/status/200"
 # endpoint = "http://httpbin.org/anything"
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api"
 
 get_response = requests.get(endpoint)
 print(get_response.text)  # print raw response
